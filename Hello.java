e233e0c81e5cf22f49d4bcc25630f540503edd1d
unknown <hieunn@gmail.com>
2023-05-02 22:24:37 +0700

    test

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+   public static void main(String[] args) {
+      Person a = new Person();
+      a.name = "Tuan";
+      a.height = 1.7f;
+      a.age = 22;
+      System.out.println(a.name);
+      System.out.println(a.height);
+      System.out.println(a.age);
+   }
+}
